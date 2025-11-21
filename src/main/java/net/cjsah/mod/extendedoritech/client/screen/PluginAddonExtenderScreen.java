556bf94a0fb0c94f58e761b8b192a38296a81f83
Cjsah <cjsah@cjsah.net>
2025-11-22 03:50:17 +0800

    update

--- src/main/java/net/cjsah/mod/extendedoritech/client/screen/PluginAddonExtenderScreen.java
+++ src/main/java/net/cjsah/mod/extendedoritech/client/screen/PluginAddonExtenderScreen.java
@@ -14,9 +14,14 @@ public class PluginAddonExtenderScreen extends AbstractContainerScreen<PluginAdd
     }
 
     @Override
+    public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
+        super.render(guiGraphics, mouseX, mouseY, partialTick);
+        this.renderTooltip(guiGraphics, mouseX, mouseY);
+    }
+
+    @Override
     protected void renderBg(GuiGraphics guiGraphics, float v, int i, int i1) {
         guiGraphics.blit(BasicMachineScreen.BACKGROUND, this.leftPos, this.topPos, 0, 0, this.imageWidth, this.imageHeight, this.imageWidth, this.imageHeight);
         guiGraphics.blit(BasicMachineScreen.ITEM_SLOT, this.leftPos + 79, this.topPos + 34, 0, 0, 18, 18, 18, 18);
-
     }
 }
