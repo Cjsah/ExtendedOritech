c5bc269cde6bf4b2817b774258749b8e6b50ef6b
Cjsah <cjsah@cjsah.net>
2025-11-22 04:12:03 +0800

    update

--- src/main/java/net/cjsah/mod/extendedoritech/init/ModBlocks.java
+++ src/main/java/net/cjsah/mod/extendedoritech/init/ModBlocks.java
@@ -1,10 +1,14 @@
 package net.cjsah.mod.extendedoritech.init;
 
 import com.tterrag.registrate.util.entry.BlockEntry;
+import net.cjsah.mod.extendedoritech.ExtendedOritech;
 import net.cjsah.mod.extendedoritech.block.PluginAddonExtenderBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.neoforged.neoforge.client.model.generators.ConfiguredModel;
+import net.neoforged.neoforge.client.model.generators.ModelFile;
+import rearth.oritech.block.blocks.addons.MachineAddonBlock;
 import rearth.oritech.block.blocks.processing.MachineCoreBlock;
 
 import static net.cjsah.mod.extendedoritech.ExtendedOritech.REGISTRATE;
@@ -24,6 +28,16 @@ public class ModBlocks {
         .block("plugin_addon_extender", PluginAddonExtenderBlock::new)
         .initialProperties(() -> Blocks.IRON_BLOCK)
         .properties(BlockBehaviour.Properties::noOcclusion)
+        .blockstate((ctx, prov) -> {
+            String name = ctx.getName();
+            ModelFile activeModel = prov.models().cubeAll(name, ExtendedOritech.of("block/" + name));
+            ModelFile noActiveModel = prov.models().cubeAll(name + "_disabled", ExtendedOritech.of("block/" + name + "_disabled"));
+            prov.getVariantBuilder(ctx.get())
+                .partialState().with(MachineAddonBlock.ADDON_USED, true)
+                .addModels(new ConfiguredModel(activeModel))
+                .partialState().with(MachineAddonBlock.ADDON_USED, false)
+                .addModels(new ConfiguredModel(noActiveModel));
+        })
         .simpleItem()
         .register();
 
--- src/main/java/net/cjsah/mod/extendedoritech/inventory/component/StackHandler.java
+++ src/main/java/net/cjsah/mod/extendedoritech/inventory/component/StackHandler.java
@@ -1,13 +1,21 @@
 package net.cjsah.mod.extendedoritech.inventory.component;
 
 import net.cjsah.mod.extendedoritech.block.entity.PluginAddonExtenderBlockEntity;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.ItemStack;
 import net.neoforged.neoforge.items.ItemStackHandler;
+import rearth.oritech.block.blocks.addons.MachineAddonBlock;
 
 public class StackHandler extends ItemStackHandler {
     private final PluginAddonExtenderBlockEntity blockEntity;
 
     public StackHandler(PluginAddonExtenderBlockEntity blockEntity) {
         this.blockEntity = blockEntity;
+    }
+
+    @Override
+    public boolean isItemValid(int slot, ItemStack stack) {
+        return stack.getItem() instanceof BlockItem blockItem && blockItem.getBlock() instanceof MachineAddonBlock;
     }
 
     @Override
